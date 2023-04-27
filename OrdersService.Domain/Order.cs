2538de2188ecfa0d4a2420a87dfec086ced13305
Aminev Ural <aminevural51@gmail.com>
2023-04-27 17:51:33 +0300

    Added structure

---
+++ OrdersService.Domain/Order.cs
@@ -0,0 +1,9 @@
+﻿namespace OrdersService.Domain;
+
+public class Order
+{
+    public Guid Id { get; set; }
+    public string Status { get; set; }
+    public DateTime Created { get; set; }
+    public List<string> orders { get; set; }
+}
